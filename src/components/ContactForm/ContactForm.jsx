a7f134a576841e62618635863fa4e72a49d8b023
Wlad <wladkrychin@gmail.com>
2021-08-19 16:46:45 +0200

    2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -29,7 +29,6 @@ render(){
   value={this.name}
   onChange={this.handleInputChange}
   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
   required
 />
         </label>
@@ -40,7 +39,6 @@ render(){
   onChange={this.handleInputChange}
   value={this.tel}
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
   required
 /></label>
 <button type="submit">Add contact</button>
